3fa7f1919df624bbb790f5e67dcb1fa4d74daf60
mesropaghumyan <mesropmkl@gmail.com>
2024-08-13 18:42:24 +0200

    underline keywords in about component

--- src/app/data.ts
+++ src/app/data.ts
@@ -31,13 +31,13 @@ export const data = {
       title: "À propos de moi",
       paragraphs: [
         {
-          text: "Je suis un développeur web passionné, spécialisé dans la conception et le développement d'applications. Mon parcours m'a permis d'explorer divers domaines de l'informatique, tels que les systèmes, les réseaux et la data, tout en perfectionnant mes compétences à travers des projets personnels, universitaires et professionnels."
+          text: "Je suis un <a target='_blank' href='https://www.onisep.fr/ressources/univers-metier/metiers/developpeur-developpeuse-informatique' class='text-slate-600 hover:text-slate-800 underline font-bold'>développeur web</a> passionné, spécialisé dans la conception et le développement d'applications. Mon parcours m'a permis d'explorer divers domaines de l'informatique, tels que la programmation, les systèmes, les réseaux et la data, tout en perfectionnant mes compétences à travers des projets personnels, universitaires et professionnels."
         },
         {
-          text: "Actuellement, je travaille en tant que développeur Full-Stack avec une approche DevOps, me concentrant sur Angular pour le web, Laravel pour les applications métiers complexes, et Node.js pour des solutions plus légères."
+          text: "Actuellement, je travaille en tant que développeur Full-Stack avec une approche DevOps, me concentrant sur <a target='_blank' href='https://angular.dev/' class='text-slate-600 hover:text-slate-800 font-bold underline'>Angular</a> pour le web, <a target='_blank' href='https://laravel.com/' class='text-slate-600 hover:text-slate-800 font-bold underline'>Laravel</a> pour les applications métiers complexes, et <a target='_blank' href='https://nodejs.org/en' class='text-slate-600 hover:text-slate-800 font-bold underline'>Node.js</a> pour des solutions plus légères."
         },
         {
-          text: "Diplômé d'un BUT Informatique à l'IUT Lyon 1, je poursuis un diplôme d'ingénieur à l'École des Mines d'Alès. Dès septembre, je rejoindrai Cosywee en tant qu'ingénieur logiciel en alternance, où je me spécialiserai en génie logiciel et gestion de projet, tout en contribuant à l'amélioration des outils internes, notamment l'ERP."
+          text: "Diplômé d'un BUT Informatique à l'<a target='_blank' href='https://iut.univ-lyon1.fr/' class='text-slate-600 hover:text-slate-800 font-bold underline'>IUT Lyon 1</a>, je poursuis un diplôme d'ingénieur à l'<a target='_blank' href='https://www.imt-mines-ales.fr/' class='text-slate-600 hover:text-slate-800 font-bold underline'>École des Mines d'Alès</a>. Dès septembre, je rejoindrai <a target='_blank' href='https://www.cosywee.com/' class='text-slate-600 hover:text-slate-800 font-bold underline'>Cosywee</a> en tant qu'ingénieur logiciel en alternance, où je me spécialiserai en génie logiciel et gestion de projet, tout en contribuant à l'amélioration des outils internes, notamment l'ERP."
         }
       ]
     }
